32daa325feda86f68788e89e58f0d6703ffec7bb
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-10-31 15:40:10 -0500

    let to use readme file at PyPi

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@
 import setuptools
 from distutils.core import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
 setup(name="OBISdat",
-      version='1.0',
+      version='1.1',
+      long_description = readme,
+      long_description_content_type='text/markdown',
       author='Ulises Rosas',
       author_email='ulisesfrosasp@gmail.com',
       url='https://github.com/Ulises-Rosas/OBISdat',
       packages = ['OBISdat'],
       package_dir = {'OBISdat': 'src'},
-      scripts = ['src/obis.py']
+      scripts = ['src/obis.py'],
+      classifiers=[
+             'Programming Language :: Python :: 3',
+             'License :: OSI Approved :: MIT License'
+             ]
       )
-
